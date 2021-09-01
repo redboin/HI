bd3cc508bf9ac263176ada134e1edc36af130715
redboin <84126390+redboin@users.noreply.github.com>
2021-09-01 15:22:43 +0900

    1

--- board/board.cpp
+++ board/board.cpp
@@ -25,9 +25,9 @@ void Board::print() {
 	cout << endl;
 }
 int main() {
-	Board::add("중간고사는 감독 없는 자율 시험입니다.");
-	Board::add("코딩 라운지 많이 이용해 주세요.");
-	Board::print();
-	Board::add("진소린 학생이 경진대회 입상하였습니다. 축하해주세요");
-	Board::print();
+ Board::add("중간고사는 감독 없는 자율 시험입니다.");
+ Board::add("코딩 라운지 많이 이용해 주세요.");
+ Board::print();
+ Board::add("진소린 학생이 경진대회 입상하였습니다. 축하해주세요");
+ Board::print();
 }
